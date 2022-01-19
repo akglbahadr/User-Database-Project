0b28121f53cc8e4cd66de5ad93e5386986ac52d0
akglbahadr <akgl.bahadr@gmail.com>
2022-01-19 23:09:17 +0300

    test fetch metod is implemented

--- client/index.js
+++ client/index.js
@@ -1,5 +1,8 @@
 //adding eventHandlers
 document.addEventListener("DOMContentLoaded", function () {
+  fetch("http://localhost:3000/getAll")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
   loadHTMLTable([]);
 });
 
